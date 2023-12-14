c7d95185613bccf8765075b509ef270ce300e51c
EduardoFariaKruger <90162399+EduardoFariaKruger@users.noreply.github.com>
2023-12-14 15:52:58 -0300

    primeiras funcoes

--- numberPuzzle.c
+++ numberPuzzle.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <ncurser.h>
 
 #define MAX 3
 
@@ -23,6 +24,103 @@ void initializeArray(int array[])
     {
         array[i] = i;
     }
+}
+
+int isInOrder(int board[MAX][MAX])
+{
+    int adder = 1;
+    for(int i = 0; i < MAX; i++)
+    {
+        for(int j = 0; j < MAX; j++)
+        {
+            if(board[i][j] != adder)
+            {
+                return 0;
+            }
+            adder++;
+            if (adder == 9)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+struct coordinates findZero(int board[MAX][MAX])
+{
+    struct coordinates zero;
+    for (int i = 0; i < MAX; i++)
+    {
+        for(int j = 0; j < MAX, j++)
+        {
+            if (board[i][j] == 0)
+            {
+                zero.x = j;
+                zero.y = i;
+                return zero;
+            }
+        }
+    }
+}
+
+void swap(int board[MAX][MAX], struct coordinates *zero, int x, int y)
+{
+    struct coordinates position;
+    position.x = x;
+    position.y = y;
+    if(isInRange(position))
+    {
+        int temp;
+        temp = board[zero.x][zero.y];
+        board[zero.x][zero.y] = board[x][y];
+        board[x][y] = temp;
+        zero->x = x;
+        zero->y = y;
+    }
+}
+
+void numberPuzzle(int board[MAX][MAX])
+{
+    int board[MAX][MAX];
+    initializeBoard(board);
+    printBoard(board);
+    struct coordinates zero = findZero(board);
+
+    initscr();
+    keypad(stdscr, TRUE);
+    timeout(0);
+
+    int key;
+
+    while (!isInOrder(board)) {
+        key = getch();
+        switch (tecla) {
+            case KEY_UP:
+                swap(board, &zero, zero.x, zero.y + 1);
+                printBoard(board);
+                printf("Seta para cima pressionada.\n");
+                break;
+            case KEY_DOWN:
+                swap(board, &zero, zero.x, zero.y - 1);
+                printBoard(board);
+                printf("Seta para baixo pressionada.\n");
+                break;
+            case KEY_LEFT:
+                swap(board, &zero, zero.x - 1, zero.y);
+                printBoard(board);
+                printf("Seta para esquerda pressionada.\n");
+                break;
+            case KEY_RIGHT:
+                swap(board, &zero, zero.x + 1, zero.y + 1);
+                printBoard(board);
+                printf("Seta para direita pressionada.\n");
+                break;
+        }
+    }
+
+    endwin();
+
 }
 
 int randomizeNumber(int array[], int *size)
@@ -69,9 +167,7 @@ void printBoard(int board[MAX][MAX])
 int main()
 {
     srand(0);
-    int board[MAX][MAX];
-    initializeBoard(board);
-    printBoard(board);
+
 
     return 0;
 }
